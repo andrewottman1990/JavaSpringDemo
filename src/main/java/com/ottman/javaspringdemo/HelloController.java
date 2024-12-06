9966f55650713ec2a6ac8e0db3bd899a9d898982
Andrew Ottman <apjottman@gmail.com>
2024-12-06 16:42:40 -0500

    Add hello world controller

---
+++ src/main/java/com/ottman/javaspringdemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.ottman.javaspringdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot 4!";
+    }
+}
