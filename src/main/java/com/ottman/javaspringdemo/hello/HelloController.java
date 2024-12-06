41856f16ba7e1cb11b56abf19692c0e9e15a9b11
Andrew Ottman <apjottman@gmail.com>
2024-12-06 16:47:20 -0500

    Move controller into feature folder

--- src/main/java/com/ottman/javaspringdemo/hello/HelloController.java
+++ src/main/java/com/ottman/javaspringdemo/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.ottman.javaspringdemo;
+package com.ottman.javaspringdemo.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
