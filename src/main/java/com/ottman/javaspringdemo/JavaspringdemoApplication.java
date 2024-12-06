f8a04a337b0665cfeef1c13ab487de51a3c9d5c9
Andrew Ottman <apjottman@gmail.com>
2024-12-06 16:25:51 -0500

    Spring Initializr files

---
+++ src/main/java/com/ottman/javaspringdemo/JavaspringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ottman.javaspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ottman/javaspringdemo/JavaspringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ottman.javaspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
